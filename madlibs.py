d242ffd02f1881baa11b512551d5cb54814e47cc
Илья Юрченко <ilyayurchenko@Ilas-MacBook-Air.local>
2022-06-10 18:46:46 +0300

    madlibs generator example

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+name = input("person name: ")
+famous_person = input("famous person name: ")
+significant_event = input("significant_event: ")
+programming_language = input("programming language: ")
+
+
+madlib = f"Hi! My name is {name} and there is my madlib program. Here can be a cool story about {famous_person} or {significant_event} but its only a bunch of words to show my {programming_language} skills"
+print(madlib)
